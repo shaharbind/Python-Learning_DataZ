9fd01f62587420c8f562110f47b0359ba958c013
shaharbind <shaharbind569@gmail.com>
2025-04-02 11:19:36 +0545

    Chapter - 6 Practice Questions Completed

---
+++ Chapter 6/Chapter 6 - PS/05_Problem5.py
@@ -0,0 +1,12 @@
+# Write a program which finds out whether a given name is present in a list or not. 
+
+name = input("Enter  name: ")
+
+list1 = ['Aman', 'Shyam', 'Hari', 'Ankit', 'Suman', 'Boy']
+
+if(name in list1):
+    print("Given name is in list!!")
+
+
+else:
+    print("Name is not in given list")
---
+++ Chapter 6/Chapter 6 - PS/07_Problem7.py
@@ -0,0 +1,10 @@
+# Write a program to find out whether a given post is talking about “Harry” or not. 
+
+post = input("Enter post content: ")
+
+if(("Arbind") in post):
+    print("Post is talking about Mr.Arbind!!")
+
+
+else:
+    print("Not talking about Arbind")
