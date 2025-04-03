1f17aee4098ef6e1401f4eab23badbd4ab95e171
shaharbind <shaharbind569@gmail.com>
2025-04-03 19:42:44 +0545

    Starting Functions

---
+++ Chapter 8/01_functions.py
@@ -0,0 +1,23 @@
+# It is a function definition
+def avg():
+    a = int(input("Enter your number: "))
+    b = int(input("Enter your number: "))
+    c = int(input("Enter your number: "))
+
+    average = (a+b+c)/3
+    print("The average is ", average)
+
+
+# It is a function call
+avg()
+avg()
+avg()
+
+# Quick Quiz:  Write a program to greet a user with “Good day” using functions.
+
+def greet():
+    print("Good Day dude!!!")
+
+
+greet()
+
---
+++ Chapter 8/02_functions_with_argument.py
@@ -0,0 +1,18 @@
+
+def greet(name):
+    print("Good Day, " + name)
+
+
+greet("Arbind")
+greet("Aman")
+
+# For two arguments
+
+def greet(name, ending):
+    print("Good Day, " + name)
+    print(ending)
+
+
+greet("Arbind","Thanks")
+greet("Aman","Thank you")
+
