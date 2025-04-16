be8b2ebfb5a8575df56c6fed31ef6be655abba29
shaharbind <shaharbind569@gmail.com>
2025-04-16 18:15:21 +0545

    Learning Chapter 8

---
+++ Chapter 8/04_default_argument.py
@@ -0,0 +1,6 @@
+def goodDay(name, ending="Thank You"): # Here nending is default value , if we will not give ending than it will take default value
+    print(f"Good Day, {name}")
+    print(ending)
+
+goodDay("Arbind")
+goodDay("Arbind", "Welcome!!!!")
