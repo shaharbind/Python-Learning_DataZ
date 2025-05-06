3704714baa9c11983337978cdab57146b4a1a82d
shaharbind <shaharbind569@gmail.com>
2025-05-06 14:47:18 +0545

    Reading Chapter - 9

---
+++ Chapter 9/Chapter 9 - PS/01_P1.py
@@ -0,0 +1,13 @@
+'''
+Write a program to read the text from a given file ‘poems.txt’ and find out 
+whether it contains the word ‘twinkle’
+
+'''
+f = open("poem.txt")
+
+content = f.read()
+if("twinkle" in content):
+    print("Twinkle is present in the content")
+else:
+    print("Twinkle is not present in the content")
+
