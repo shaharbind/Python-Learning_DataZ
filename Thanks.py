73d9ab665ca6995bd17cb81011bd34711d64ad8d
shaharbind <shaharbind569@gmail.com>
2025-08-22 16:12:22 +0545

    Thanks to code with harry

---
+++ Thanks.py
@@ -0,0 +1,24 @@
+# Thanks.py
+"""
+This file is dedicated to Super Herozzz of Coding "Code With Harry" for providing
+valuable Python tutorials and helping me complete my Python learning journey
+from basics to Object-Oriented Programming (OOP) and beyond.
+
+I have successfully learned all chapters up to Chapter 11:
+1. Basics of Python
+2. Strings & Lists
+3. Conditional Statements & Loops
+4. Functions & Recursion
+5. File Handling
+6. Object-Oriented Programming (OOP)
+7. Inheritance & Operator Overloading
+
+Throughout my learning, I made regular commits and pushed my progress to GitHub.
+Now, I am ready to start my AI Chatbot Mega Project, thanks to the solid foundation
+built with Code With Harry tutorials.
+
+Thank you, Code With Harry!
+- Arbind Shah
+"""
+
+print("A big thanks to hero of coding Code With Harry for guiding me throughout my Python journey!")
